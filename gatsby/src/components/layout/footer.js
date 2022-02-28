b173a37e08683a577534acad30b958a3c8d7c9ea
Luis B <luisbttf@gmail.com>
2022-02-27 23:34:13 -0600

    footer

--- gatsby/src/components/layout/footer.js
+++ gatsby/src/components/layout/footer.js
@@ -1,10 +1,42 @@
 import React from 'react'
+import { useStaticQuery, graphql } from "gatsby";
+import styled from 'styled-components';
 
 
 const Footer = () => {
+    const data = useStaticQuery(graphql`
+query {
+    sanitySettingsPage {
+        logoBlanco2 {
+        alt
+        asset {
+            gatsbyImageData(
+            layout: FULL_WIDTH
+            outputPixelDensities: 1.5
+            placeholder: BLURRED
+            )
+        }
+        }
+    }
+}
+`);
     return (
-        <p>Footer</p>
+        <FooterContainer>
+            <div className='iz'>
+
+            </div>
+            <div className='ce'>
+
+            </div>
+            <div className='de'>
+
+            </div>
+        </FooterContainer>
     )
 }
+
+const FooterContainer = styled.footer`
+
+`
 
 export default Footer
