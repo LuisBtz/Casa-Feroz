9a5f60e9265af009c68503549a17648cdbf756b5
Luis B <luisbttf@gmail.com>
2022-03-01 09:26:16 -0600

    footer

--- gatsby/src/components/layout/footer.js
+++ gatsby/src/components/layout/footer.js
@@ -105,6 +105,98 @@ const FooterContainer = styled.footer`
     background-color: var(--black);
     color: var(--white);
     display: flex;
+    justify-content: space-between;
+    padding: 50px 20px;
+    @media (max-width: 780px) {
+        flex-direction: column;
+        text-align: center;
+        padding-bottom: 100px !important;
+        .iz {
+            height: auto !important;
+            .logo {
+                margin: 0 auto 50px;
+            }
+        }
+        .ce {
+            height: auto !important;
+            margin: 50px auto;
+            width: 100% !important;
+            padding: 0 20px !important;
+            .linksFooter  {
+                grid-template-columns: repeat(2) !important;
+                justify-content: center;
+                text-align: center;
+            }
+            .warning {
+                margin-top: 50px;
+            }
+        }
+        .de {
+            text-align: center !important;
+            .redes {
+                justify-content: center !important;
+                a {
+                    margin: 0 20px !important;
+                }
+            }
+        }
+    }
+    .iz {
+        width: auto;
+        height: 260px;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        .logo {
+            width: 150px;
+        }
+    }
+    .ce {
+        width: 65%;
+        padding: 0 50px;
+        height: 260px;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        .linksFooter {
+            display: grid;
+            grid-template-columns: repeat(2, 250px);
+            justify-content: center;
+            @media (max-width: 880px) {
+                grid-template-columns: repeat(2, 180px);
+            }
+            .link {
+                margin-bottom: 10px;
+                a, p {
+                    text-transform: uppercase;
+                    font-family: var(--bold);
+                }
+            }
+        }
+        .warning {
+            text-align: center;
+        }
+    }
+    .de {
+        width: auto;
+        text-align: right;
+        font-family: var(--bold);
+        text-transform: uppercase;
+        .social {
+            .redes {
+                display: flex;
+                justify-content: end;
+                margin-top: 20px;
+                margin-bottom: 85px;
+                a {
+                    display: block;
+                    margin-left: 20px;
+                    width: 23px;
+                }
+            }
+        }
+    }
+
 `
 
 export default Footer
